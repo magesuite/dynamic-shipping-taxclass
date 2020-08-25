0b1109df1b00db4a2c8052c709d3c9916a138c99
Adam Karnowka <adam.karnowka@creativestyle.pl>
2020-08-25 11:05:38 +0200

    Initial release

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Magesuite_DynamicShippingTaxclass',
+    __DIR__
+);
+
