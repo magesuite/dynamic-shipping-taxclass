1d1762fda9a67d96ce795758149b103f6f041131
Iwona Jóźwiak <iwona.jozwiak@creativestyle.pl>
2023-10-02 07:58:16 +0000

    Revert "fix: [IPET-2176] Fix for the issue reported by error: Exception message: Infinite loop detected"
    This reverts commit b45174eb0156acc061d35b5d6f2020e95d851afd

--- Model/Command/GetQuoteItems.php
+++ Model/Command/GetQuoteItems.php
@@ -4,23 +4,30 @@ namespace Magesuite\DynamicShippingTaxclass\Model\Command;
 
 class GetQuoteItems
 {
-    protected \Magento\Checkout\Model\Cart $customerCart;
+    protected \Magento\Checkout\Model\Session $checkoutSession;
+
+    protected \Magento\Quote\Model\ResourceModel\Quote\Item\CollectionFactory $quoteItemCollectionFactory;
 
     public function __construct(
-        \Magento\Checkout\Model\Cart $customerCart
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Quote\Model\ResourceModel\Quote\Item\CollectionFactory $quoteItemCollectionFactory
     ) {
 
-        $this->customerCart = $customerCart;
+        $this->checkoutSession = $checkoutSession;
+        $this->quoteItemCollectionFactory = $quoteItemCollectionFactory;
     }
 
     public function execute()
     {
-        try {
-            $quote = $this->customerCart->getQuote();
+        $quoteId = $this->checkoutSession->getQuoteId();
 
-            return $quote->getAllItems();
-        } catch (\Exception $e) {
-            return [];
+        if (empty($quoteId)) {
+            return $this->checkoutSession->getQuote()->getAllItems();
         }
+
+        $quoteItemCollection = $this->quoteItemCollectionFactory->create();
+        $quoteItemCollection->addFieldToFilter('quote_id', $quoteId);
+
+        return $quoteItemCollection;
     }
 }
